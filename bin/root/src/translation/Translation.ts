bec41cdd39328fb57fe2ca36b710c0778cc258ee
Niels Steenbeek <nsteenbeek@hso.com>
2019-09-24 11:47:45 +0200

    Refactoring Translation

--- bin/root/src/translation/Translation.ts
+++ bin/root/src/translation/Translation.ts
@@ -1,46 +1,55 @@
+// @ts-ignore
 import i18next from 'i18next';
-import XHR from 'i18next-xhr-backend';
+import {WebApi} from '../WebApi/WebApi';
+
+declare interface Resources {
+    [index: string]: {translation: {[index: string]: string}};
+}
+
+declare interface Options {
+    relativePath: string;
+    fileExtension?: string;
+}
 
 export class Translation {
-    static init(options: {relativePath: string; fileExtension?: string} = {relativePath: null, fileExtension: null}): Promise<void> {
+    public static async init(options: Options = {relativePath: null, fileExtension: null}): Promise<void> {
         const globalContext = Xrm.Utility.getGlobalContext(),
-            lng = globalContext.userSettings.languageId.toString();
-        return new Promise((resolve, reject): void => {
-            if (!options.relativePath) {
-                reject(`Please specify relativePath like 'new_'`);
-                return;
-            }
-            i18next
-                .use(XHR)
-                .init({
-                    lng: lng,
-                    fallbackLng: '1033',
-                    backend: {
-                        loadPath: this.getLoadPath(options.relativePath, options.fileExtension)
-                    }
-                }, (err: string[]) => {
-                    if (err) {
-                        console.log(err.join('/n'));
-                    }
-                    resolve();
-                });
+            lng = globalContext.userSettings.languageId.toString(),
+            resources = await Translation.getResources(options);
+        await i18next.init({
+            lng: lng,
+            resources: resources,
+            fallbackLng: '1033',
         });
     }
 
-    static getLoadPath(relativePath = '', fileExtension = 'json'): string {
+    private static async getResources(options: Options): Promise<Resources> {
         const globalContext = Xrm.Utility.getGlobalContext(),
-            clientUrl = globalContext.getClientUrl();
-        let path = relativePath;
-        path = path.startsWith('.') ? path.substr(1) : path;
-        path = !path.startsWith('/') ? `/${path}` : path;
-        return `${clientUrl}/WebResources/${path}/{{lng}}.${fileExtension}`;
+            lng = globalContext.userSettings.languageId.toString(),
+            resources: Resources = {},
+            translation = await Translation.getTranslation(lng, options);
+        if (translation) {
+            resources[lng] = translation;
+        }
+        if (lng !== '1033') {
+            resources['1033'] = await Translation.getTranslation('1033', options);
+        }
+        return resources;
     }
 
-    static translate(text: string, options?: i18next.TOptions | string): string {
+    private static async getTranslation(languageId: string, options: Options): Promise<{translation: {}}> {
+        const fileJson = await WebApi.request('GET', `${options.relativePath}/${languageId}.json`);
+        if (fileJson) {
+            const resource = JSON.parse(fileJson.response);
+            return {translation: resource};
+        }
+    }
+
+    public static translate(text: string, options?: i18next.TOptions | string): string {
         return i18next.t(text, options);
     }
 
-    static translateArray(text: string | Array<string>, options?: i18next.TOptions | string): Array<string> {
+    public static translateArray(text: string | Array<string>, options?: i18next.TOptions | string): Array<string> {
         if (text instanceof Array) {
             const translations: string[] = [];
             for (const txt of text) {
