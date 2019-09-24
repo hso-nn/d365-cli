5c680b59bbf77a9d90c43e12b033b901a5e79271
Niels Steenbeek <nsteenbeek@hso.com>
2019-09-24 16:47:42 +0200

    Add Translation

--- bin/Entity/Entity.form.ts
+++ bin/Entity/Entity.form.ts
@@ -1,5 +1,9 @@
+import {Translation} from '../translation/Translation';
 
 export class EntityForm {
     static onLoad(executionContext: Xrm.Events.EventContext): void {
+        Translation.init({
+            relativePath: '<%= publisher %>_/<%= projectabbr %>/locales'
+        });
     }
 }
