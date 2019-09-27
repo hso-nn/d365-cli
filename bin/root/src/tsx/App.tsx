e5b50adbc998a59d418eb1e086f0063795d45359
Niels Steenbeek <nsteenbeek@hso.com>
2019-09-27 10:39:45 +0200

    Prepare for React

---
+++ bin/root/src/tsx/App.tsx
@@ -0,0 +1,16 @@
+import {Component} from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AppState {}
+
+export interface AppProps {
+    globalContext: Xrm.GlobalContext;
+}
+
+export class App<P extends AppProps, S extends AppState> extends Component<P, S> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    private get globalContext(): Xrm.GlobalContext {
+        return this.props.globalContext;
+    }
+}
