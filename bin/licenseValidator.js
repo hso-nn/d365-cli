3d75d3a1f80a1fa5ba6a387ccbcced2cbb6fe37e
Niels Steenbeek <nsteenbeek@hso.com>
2019-09-30 15:05:34 +0200

    LicenseValidator added

--- bin/licenseValidator.js
+++ bin/licenseValidator.js
@@ -1,12 +1,10 @@
 #! /usr/bin/env node
 const shell = require("shelljs");
-const readline = require('readline');
 const colors = require('colors');
-const inquirer = require('inquirer');
 const variables = require("./variables");
 
 module.exports = {
-    generateLicense(licensename) {
+    generateLicenseValidator(licensename) {
         if (shell.test('-e', `src/License`)) {
             console.log(colors.red(`src/License already exists!`));
         } else if (process.argv[5]) {
@@ -18,25 +16,19 @@ module.exports = {
 };
 
 const generate = async (licensename) => {
-    console.log(`Adding D365 License ${licensename}...`);
+    console.log(`Adding D365 License Validator for ${licensename}...`);
     variables.get(async ({publisher, projectabbr}) => {
-        const answers = await inquirer.prompt([{
-            type: 'input',
-            name: 'validateLicenseAction',
-            message: 'Validate License Action',
-            default: 'hds_DLFValidateLicense'
-        }]);
-
         shell.mkdir(`src/License`);
         shell.ls(`${__dirname}/License/*.*`).forEach(function (file) {
             const split = file.split('/');
             const filename = split[split.length - 1];
             shell.cp('-r', file, `src/License`);
             shell.sed('-i', new RegExp('<%= licensename %>', 'ig'), licensename, `src/License/${filename}`);
-            shell.sed('-i', new RegExp('<%= validateLicenseAction %>', 'ig'), answers.validateLicenseAction, `src/License/${filename}`);
             shell.sed('-i', new RegExp('<%= publisher %>', 'ig'), publisher, `src/License/${filename}`);
             shell.sed('-i', new RegExp('<%= projectabbr %>', 'ig'), projectabbr, `src/License/${filename}`);
         });
-        console.log("Adding D365 License done");
+        const webpackConfigFile = shell.ls('webpack.config.js')[0];
+        shell.sed('-i', 'entry: {', `entry: {\n        LicenseValidator: [\n            path.resolve(__dirname, "src/License/Validator.ts")\n        ],`, webpackConfigFile);
+        console.log("Adding D365 License Validator done");
     });
 };
