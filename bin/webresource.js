313c3ea5425137e0198a2f03a6c5ddd74d80ca04
Niels Steenbeek <nsteenbeek@hso.com>
2019-09-30 13:32:58 +0200

    Inquirer for Template

--- bin/webresource.js
+++ bin/webresource.js
@@ -2,6 +2,7 @@
 const shell = require("shelljs");
 const colors = require('colors');
 const variables = require("./variables");
+const inquirer = require('inquirer');
 
 module.exports = {
     generateWebresource(webresourcename) {
@@ -17,13 +18,22 @@ module.exports = {
     }
 };
 
-const generate = (webresourcename) => {
-    console.log(`Adding D365 Webresource ${webresourcename}...`);
+const generate = async (webresourcename) => {
+    console.log(`Adding D365 Webresource ${webresourcename}...abc`);
+    const answers = await inquirer.prompt([{
+            type: 'list',
+            name: 'template',
+            message: 'Which template do you want?',
+            choices: [
+                'HTML',
+                'React'
+            ]
+        }]),
+        template = answers.template;
     variables.get(({publisher, projectabbr}) => {
-        const webpackConfigFile = shell.ls('webpack.config.js')[0];
-        shell.sed('-i', 'entry: {', `entry: {\n        ${webresourcename}: [\n            path.resolve(__dirname, "src/${webresourcename}/${webresourcename}.ts")\n        ],`, webpackConfigFile);
         shell.mkdir(`src/${webresourcename}`);
-        shell.ls(`${__dirname}/Webresource/*.*`).forEach(function (file) {
+        const srcDir = `${__dirname}/Webresource${template === 'React' ? 'Tsx' : ''}/*.*`;
+        shell.ls(srcDir).forEach(function (file) {
             const split = file.split('/');
             const filename = split[split.length - 1];
             const newfilename = filename.replace(/Webresource/g, webresourcename);
@@ -34,6 +44,8 @@ const generate = (webresourcename) => {
             shell.sed('-i', new RegExp('PUBLISHER', 'ig'), publisher, `src/${webresourcename}/${newfilename}`);
             shell.sed('-i', new RegExp('PROJECTABBR', 'ig'), projectabbr, `src/${webresourcename}/${newfilename}`);
         });
+        const webpackConfigFile = shell.ls('webpack.config.js')[0];
+        shell.sed('-i', 'entry: {', `entry: {\n        ${webresourcename}: [\n            path.resolve(__dirname, "src/${webresourcename}/${webresourcename}.ts")\n        ],`, webpackConfigFile);
         console.log("Adding D365 Webresource done");
     });
 };
