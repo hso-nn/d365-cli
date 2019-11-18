0dcb0fcb960742eba6edf6e951cb2820130436d0
Niels Steenbeek <nsteenbeek@hso.com>
2019-11-18 11:43:43 +0100

    Better placeholders for LicenseValidator template

--- src/LicenseValidator.ts
+++ src/LicenseValidator.ts
@@ -19,7 +19,7 @@ export class LicenseValidator {
     private static async generate(licensename: string): Promise<void> {
         console.log(`Adding D365 License Validator for ${licensename}...`);
         shell.exec('npm install --save dlf-core@latest');
-        const {publisher, projectabbr} = await Variables.get();
+        const {publisher, namespace} = await Variables.get();
         shell.mkdir(`src/License`);
         shell.ls(`${__dirname}/License/*.*`).forEach(function (file) {
             const split = file.split('/');
@@ -27,7 +27,7 @@ export class LicenseValidator {
             shell.cp('-r', file, `src/License`);
             shell.sed('-i', new RegExp('<%= licensename %>', 'ig'), licensename, `src/License/${filename}`);
             shell.sed('-i', new RegExp('<%= publisher %>', 'ig'), publisher, `src/License/${filename}`);
-            shell.sed('-i', new RegExp('<%= projectabbr %>', 'ig'), projectabbr, `src/License/${filename}`);
+            shell.sed('-i', new RegExp('<%= namespace %>', 'ig'), namespace, `src/License/${filename}`);
         });
         const webpackConfigFile = shell.ls('webpack.config.js')[0];
         shell.sed('-i', 'entry: {', `entry: {\n        LicenseValidator: [\n            path.resolve(__dirname, "src/License/Validator.ts")\n        ],`, webpackConfigFile);
