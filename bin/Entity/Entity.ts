a646101aea8daa148dda1a9db67522799461d464
Niels Steenbeek <nsteenbeek@hso.com>
2019-11-18 11:42:06 +0100

    Better placeholders for Entity template

--- bin/Entity/Entity.ts
+++ bin/Entity/Entity.ts
@@ -1,14 +1,14 @@
 import {EntityForm} from './Entity.form';
 
 export const Form = {
-    // In D365 Form specify Form onLoad function: <%= publisher %>.<%= projectabbr %>.Entity.Form.onLoad
+    // In D365 Form specify Form onLoad function: <%= publisher %>.<%= namespace %>.Entity.Form.onLoad
     onLoad: async (executionContext: Xrm.Events.EventContext): Promise<void> => {
         return EntityForm.onLoad(executionContext);
     }
 };
 
 export const Ribbon = {
-    // In Ribbon WorkBench specify function: <%= publisher %>.<%= projectabbr %>.Entity.Ribbon.myRibbonMethod
+    // In Ribbon WorkBench specify function: <%= publisher %>.<%= namespace %>.Entity.Ribbon.myRibbonMethod
     // In Ribbon WorkBench specify Xrm Parameter 'Primary Control', which is formContext
     myRibbonMethod: (formContext: Xrm.FormContext): void => {
         // EntityForm.myFormMethod(formContext);
