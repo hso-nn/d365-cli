5864a2e65aa84b6b5a1c3cf20407421fab9f2e7a
Niels Steenbeek <nsteenbeek@hso.com>
2020-01-22 17:07:58 +0100

    Update and clean package.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,8 @@ const path = require("path"),
     dir_build = path.resolve(__dirname, "bin"),
     WebpackAutoInject = require("webpack-auto-inject-version"),
     CopyWebpackPlugin = require("copy-webpack-plugin"),
-    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
     UglifyJSPlugin = require("uglifyjs-webpack-plugin"),
     nodeExternals = require("webpack-node-externals");
-
-const postcssLoader = {
-        loader: "postcss-loader",
-        options: {
-            plugins: function () {
-                return [
-                    require("autoprefixer")({
-                        browsers: ["Android > 0","last 3 versions", "> 1%"]
-                    }),
-                    require("cssnano")({zindex: false})
-                ];
-            }
-        }
-    },
-    scssLoaders = [DEBUG ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", postcssLoader, "sass-loader"],
-    cssLoaders = [DEBUG ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", postcssLoader];
 
 module.exports = {
     mode: mode,
@@ -46,39 +29,11 @@ module.exports = {
         filename: "[name].js"
     },
     resolve: {
-        extensions: [".js", ".json", ".ts"]
+        extensions: [".json", ".ts"]
     },
     module: {
         rules: [{
-            enforce: "pre",
-            test: /\.js?$/,
-            loader: "eslint-loader",
-            options: {
-                failOnWarning: false,
-                failOnError: true
-            },
-            include: [
-                path.resolve(__dirname, "./src")
-            ],
-            exclude: [
-                path.resolve(__dirname, "./src/libs")
-            ]
-        },{
-            loader: "babel-loader",
-            test: /\.js$/,
-            include: [
-                path.resolve(__dirname, "./src"),
-                path.resolve(__dirname, "./tests")
-            ],
-            exclude: [
-                path.resolve(__dirname, "./src/libs")
-            ]
-        }, {
             test: /\.json$/, loader: "json-loader"
-        }, {
-            test: /\.scss$/, loader: scssLoaders
-        }, {
-            test: /\.css$/, loader: cssLoaders
         }, {
             test: /\.ts$/,
             enforce: "pre",
@@ -107,12 +62,6 @@ module.exports = {
         }),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         new webpack.BannerPlugin("HSO D365 CLI [AIV]{version}[/AIV] | (c) HSO Innovation"),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: "[name]/[name].css",
-            chunkFilename: "[id].css",
-        })
     ].concat(DEBUG ? [] : [
         new CopyWebpackPlugin([{
             from: "./**/**.png",
@@ -130,10 +79,6 @@ module.exports = {
             from: "./root/**/**.json",
             to: dir_build,
             context: "src"
-        }, {
-            from: "locales/*",
-            to: dir_build,
-            context: "src/translation"
         }]),
     ]),
     stats: {
