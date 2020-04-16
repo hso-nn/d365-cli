0fe444e60b6fcb79c6d2016939ba594684a6d5fa
Niels Steenbeek <nsteenbeek@hso.com>
2020-04-16 16:19:26 +0200

    Set version 1.6.1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {Generator} from './generator/Generator';
 import {Variables} from './Variables';
 
 program
-    .version('1.6.0') // .version(require('../package').version)
+    .version('1.6.1') // .version(require('../package').version)
     .usage('<command> [options]');
 
 program
