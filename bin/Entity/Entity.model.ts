851a68dd4296fdf9819ad729b3838d866312b90d
Niels Steenbeek <nsteenbeek@hso.com>
2021-07-29 14:34:04 +0200

    Disable eslint no-unused-vars

--- bin/Entity/Entity.model.ts
+++ bin/Entity/Entity.model.ts
@@ -1,6 +1,7 @@
 /* This is a generated file, please regenerate and do not modify */
 /* eslint-disable max-len  */
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface EntityModel extends Model {}
 
 /* eslint-enable max-len  */
