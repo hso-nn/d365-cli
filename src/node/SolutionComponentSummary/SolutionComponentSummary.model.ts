1a79c2fb47f2c0e4cdd27f27820e8ff39268f6a7
Niels Steenbeek <nsteenbeek@hso.com>
2023-03-01 13:57:00 +0100

    Add msdyn_name

--- src/node/SolutionComponentSummary/SolutionComponentSummary.model.ts
+++ src/node/SolutionComponentSummary/SolutionComponentSummary.model.ts
@@ -1,6 +1,7 @@
 
 export interface SolutionComponentSummaryModel extends Model {
     msdyn_solutioncomponentsummaryid?: string;
+    msdyn_name?: string;
     msdyn_objectid?: string;
     // https://learn.microsoft.com/en-us/power-apps/developer/data-platform/reference/entities/solutioncomponent#BKMK_ComponentType
     msdyn_componenttype?: string;
